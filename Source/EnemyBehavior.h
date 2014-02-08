5f67b2c718bbb1cc26c0fb77ca4bb9f6eaa5faf4
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2014-02-08 19:56:29 +0100

    Added attack range

--- Source/EnemyBehavior.h
+++ Source/EnemyBehavior.h
@@ -28,8 +28,10 @@ public:
 	void update(float t, Enemy *const TheEnemy)
 	{
 		Vec2F Distance = Target->Position - TheEnemy->Position;
-		TheEnemy->TargetDirection = Approach<Angle, double>(TheEnemy->TargetDirection, Distance.getAngle(), t / 2.0f);		
-		TheEnemy->Acceleration = TheEnemy->TargetDirection.toDirection() * Speed;		
+		if (Distance.getLength() < 500.0) {
+			TheEnemy->TargetDirection = Approach<Angle, double>(TheEnemy->TargetDirection, Distance.getAngle(), t / 2.0f);		
+			TheEnemy->Acceleration = TheEnemy->TargetDirection.toDirection() * Speed;
+		}		
 	}
 };
 
