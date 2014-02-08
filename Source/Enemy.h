437635a3578783b6d7285302382eaf1bd1750db4
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2014-02-08 15:54:20 +0100

    Added enemy header file

---
+++ Source/Enemy.h
@@ -0,0 +1,22 @@
+//  Copyright (C) 2014 Miguel Gonzalez <miguel-gonzalez@gmx.com>
+//  Licensed under the terms of the WTFPL.
+//
+//  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+//  0. You just DO WHAT THE FUCK YOU WANT TO.
+
+#pragma once
+
+#include <Springbok/Geometry/Angle.h>
+#include <Springbok/Graphics/Image.h>
+#include "PhysicsObject.h"
+
+class Enemy : public PhysicsObject
+{
+private:
+      Angle MovementDirection = 0;
+      Angle TargetDirection = 0;
+      float AcclerateFactor = 0.0f;
+      Image Sprite;
+public:
+      Enemy() : Sprite("Enemy/Enemy01.png") {};
+}
