8883a3feb1ca5de8553ca5d2255d920af10be02e
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2014-02-19 19:41:50 +0100

    Improved UI

--- Source/UI.cpp
+++ Source/UI.cpp
@@ -12,6 +12,10 @@ void EnemyArrow::draw(RenderContext* Context)
 {
 	for(Ship* ship : Space->Ships)
 	{
+		// Skip the own ship
+		if (ship == Space->Player)
+			continue;
+		
 		Vec2F EnemyPosition = ship->Position;
 		Vec2F Direction = ship->Position - Space->Player->Position;
 		float Length = Direction.getLength();
@@ -19,10 +23,12 @@ void EnemyArrow::draw(RenderContext* Context)
 			
 		Context->Offset = (Space->ScreenSize / 2) + Normalized * ((Space->ScreenSize.Y / 3.2f) + (20000.0f / (Length / 5)));
 		Context->Rotation = Angle(Direction);
+		Context->Scale = 200.0 / Length + 0.5f;
 		float Opacity = 150.0 / Length;
 			
-		if (Length < Space->ScreenSize.X / 1.5f) {
-			Opacity = Length / 10000.0f;
+		if (Length < Space->ScreenSize.X / 2.f) {
+			Opacity = Length / (Space->ScreenSize.X / 1.3f) - 0.5f;
+			Context->Offset = (Space->ScreenSize / 2) + (Direction * 0.8f);
 		}
 			
 		Context->setColor(ship->FactionColor, Opacity);
